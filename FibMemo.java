6621b277c5593a2351e47d8614bb9c3a56fb9913
tylerking406 <tylerking406@gmail.com>
2024-10-16 00:09:53 +0200

    I used Dp in Btmup and memo method

--- FibMemo.java
+++ FibMemo.java
@@ -1,17 +1,26 @@
+import java.util.Arrays;
+
 /**
  * FibMemo
  */
 public class FibMemo {
 
-    private static int fib(int n){
+    private static int fib(int n,int[] memo){
         if(n<=1)
             return n;
-        else
-            return fib(n-1) + fib(n-2);
+        if(memo[n]!= -1)
+            return memo[n];
+        else{
+            memo[n]= fib(n-1,memo) + fib(n-2,memo);
+            return memo[n];
+        }
     }
 
     public static void main(String[] args) {
         int n = 5;
-        System.out.printf("Fib(%d)=%d\n",n,fib(n));
+        int[] memo = new int[n+1];
+        Arrays.fill(memo,-1);
+
+        System.out.printf("Fib(%d)=%d\n",n,fib(n,memo));
     }
 }
