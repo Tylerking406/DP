adcd9554cd9defb14d2e868290f402fdd653dc18
tylerking406 <tylerking406@gmail.com>
2024-10-15 23:14:08 +0200

    basic fib

--- FibMemo.java
+++ FibMemo.java
@@ -3,5 +3,15 @@
  */
 public class FibMemo {
 
-    
+    private static int fib(int n){
+        if(n<=1)
+            return n;
+        else
+            return fib(n-1) + fib(n-2);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.printf("Fib(%d)=%d\n",n,fib(n));
+    }
 }
